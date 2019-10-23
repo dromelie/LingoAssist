4049ec9fcc015a26c094dce3677fb2a6d453356c
unknown <dromelie@nyit.edu>
2019-10-22 23:22:01 -0400

    finished first tutorial for chat application

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -22,15 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
         myDatabase = FirebaseDatabase.getInstance().getReference("Message");
 
-        final TextView mytext = findViewById(R.id.text1);
+        final TextView myText = findViewById(R.id.textbox);
 
-        myDatabase.addValueEventListener(new ValueEventListener(){
-            public void onDataChange(DataSnapshot dataSnapshot){
-                mytext.setText(dataSnapshot.getValue().toString());
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myText.setText(dataSnapshot.getValue().toString());
             }
 
-            public void onCancelled(DatabaseErrorHandler databaseError){
-                mytext.setText("CANCELLED");
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                myText.setText("CANCELLED");
             }
         });
     }
@@ -38,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view){
         EditText myEditText = findViewById(R.id.editText2);
 
-        myDatabase.setValue(myEditText.getText());
+        myDatabase.setValue(myEditText.getText().toString());
         myEditText.setText("");
     }
 }
